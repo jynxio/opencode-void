4a7966bf4a08de2fdb9bb8732b5d6032363e94e6
jynxio <jinxiaomatrix@gmail.com>
2026-04-12 02:33:58 +0800

    fix: Bilingual Command Plugin prompt loss issue

--- src/_commands/bilingual/index.ts
+++ src/_commands/bilingual/index.ts
@@ -7,28 +7,28 @@ import prompt from "./_prompt";
 type CommandConfig = ValueOf<NonNullable<Config["command"]>>;
 
 const PRESET_COMMAND_CONFIG = {
-  description: "Response refinement",
-  template: prompt,
+    description: "Response refinement",
+    template: prompt,
 } satisfies CommandConfig;
 
 function createPlugin(
-  commandName: string = "bilingual",
-  commandConfig: CommandConfig = { template: "" },
+    commandName: string = "bilingual",
+    commandConfig: Partial<CommandConfig> = {},
 ) {
-  const mergedCommandConfig = {
-    ...PRESET_COMMAND_CONFIG,
-    ...commandConfig,
-  } satisfies CommandConfig;
+    const mergedCommandConfig = {
+        ...PRESET_COMMAND_CONFIG,
+        ...commandConfig,
+    } satisfies CommandConfig;
 
-  const plugin: Plugin = async () => {
-    return {
-      config: async (input) => {
-        (input.command ||= {})[commandName] = mergedCommandConfig;
-      },
+    const plugin: Plugin = async () => {
+        return {
+            config: async (input) => {
+                (input.command ||= {})[commandName] = mergedCommandConfig;
+            },
+        };
     };
-  };
 
-  return plugin;
+    return plugin;
 }
 
 export { createPlugin };
